12d788efebef8e12e0c0a875108495fff9b4261b
Matheus Matias <matheus.matias.viana@hotmail.com>
2021-12-28 10:35:12 -0300

    project started

---
+++ src/main/java/com/github/matheusmv/fileuploaddownload/FileUploadDownloadApplication.java
@@ -0,0 +1,13 @@
+package com.github.matheusmv.fileuploaddownload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileUploadDownloadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileUploadDownloadApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/matheusmv/fileuploaddownload/FileUploadDownloadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.matheusmv.fileuploaddownload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileUploadDownloadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
